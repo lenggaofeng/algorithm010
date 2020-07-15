0f859158175fe065de3672b2bfeda7f784f8d0b1
lenggaofeng <lenggaofeng@hortorgames.com>
2020-07-15 22:20:50 +0800

    daily

---
+++ build/Daily/96.不同的二叉搜索树.js
@@ -0,0 +1,11 @@
+"use strict";
+/*
+ * @lc app=leetcode.cn id=96 lang=typescript
+ *
+ * [96] 不同的二叉搜索树
+ */
+// @lc code=start
+function numTrees(n) {
+}
+;
+// @lc code=end
--- build/datastruct/BinarySearchTree.js
+++ build/datastruct/BinarySearchTree.js
@@ -1,4 +1,5 @@
 "use strict";
 /**
- * 二叉搜索树
+ * 参考链接: https://zh.wikipedia.org/wiki/%E4%BA%8C%E5%85%83%E6%90%9C%E5%B0%8B%E6%A8%B9
+
  */ 
