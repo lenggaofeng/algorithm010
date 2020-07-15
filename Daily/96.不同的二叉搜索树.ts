6018f8028c72c446f63f842514e9b6d282ea3309
lenggaofeng <lenggaofeng@hortorgames.com>
2020-07-15 22:46:58 +0800

    daily

--- Daily/96.不同的二叉搜索树.ts
+++ Daily/96.不同的二叉搜索树.ts
@@ -6,7 +6,12 @@
 
 // @lc code=start
 function numTrees(n: number): number {
-
+    let c = 1;
+    for(let i = 0; i < n; i++){
+        c = Math.floor(c * 2 * (2 * i + 1) / (i + 2));
+    }
+    return c;
 };
+
 // @lc code=end
 
--- build/Daily/96.不同的二叉搜索树.js
+++ build/Daily/96.不同的二叉搜索树.js
@@ -6,6 +6,11 @@
  */
 // @lc code=start
 function numTrees(n) {
+    let c = 1;
+    for (let i = 0; i < n; i++) {
+        c = Math.floor(c * 2 * (2 * i + 1) / (i + 2));
+    }
+    return c;
 }
 ;
 // @lc code=end
