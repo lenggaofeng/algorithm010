97f14a918e804d2d73d2c9205a9b52b96f5a237f
lgfng <747133768@qq.com>
2020-07-19 23:35:53 +0800

    daily

---
+++ Week06/Medium/65-minimum-path-sum.ts
@@ -0,0 +1,30 @@
+/**
+ * 
+ * https://leetcode-cn.com/problems/minimum-path-sum/
+ */
+
+ /**
+  * 状态转移方程：
+  * dp[i][j] = min(dp[i][j-1], dp[i-1][j]) + num[i][j]
+  */
+
+function minPathSum(grid: number[][]): number {
+    let colCnt = grid[0].length;
+    if(colCnt == 0){return 0;}
+    let rowCnt = grid.length;
+    let dp = new Array(colCnt).fill(0);
+    for(let i = 0; i < rowCnt; i++){
+        let row = grid[i];
+        dp[0] += row[0];
+        for(let j = 1; j < colCnt; j++){
+            if(i == 0){
+                dp[j] = dp[j - 1] + row[j];
+            } else {
+                dp[j] = Math.min(dp[j - 1], dp[j]) + row[j];
+            }
+        }
+    }
+    return dp[colCnt - 1];
+};
+
+// console.log(minPathSum([[1,3,1],[1,5,1],[4,2,1]]))
---
+++ build/Week06/Medium/91-decode-ways.js
@@ -0,0 +1,49 @@
+"use strict";
+/**
+ * https://leetcode-cn.com/problems/decode-ways/
+ */
+/**
+*
+* 思考重复子问题，
+* 令 f[s] 为解的总数。
+* 解总数的增长源于 1-6 与前方数字组合的二义性。
+*
+* s[i] =='0', f[i] = f[i-2]
+* s[i-1] =='1', f[i] = f[i - 1] + f[i - 2]
+* s[i-1] == '2', f[i] = s[i]=='1-6' ? f[i-1] + f[i-2]
+*
+*/
+function numDecodings(s) {
+    if (s[0] == '0') {
+        return 0;
+    }
+    let len = s.length;
+    let pre = 1;
+    let cur = 1;
+    for (let i = 1; i < len; i++) {
+        let tmp = cur;
+        let c = s[i];
+        let pc = s[i - 1];
+        if (c == '0') {
+            if (pc == '1' || pc == '2') {
+                tmp = pre;
+            }
+            else {
+                return 0;
+            }
+        }
+        else if (pc == '1') {
+            tmp = pre + cur;
+        }
+        else if (pc == '2') {
+            if (c >= '1' && c <= '6') {
+                tmp = pre + cur;
+            }
+        }
+        pre = cur;
+        cur = tmp;
+    }
+    return cur;
+}
+;
+// console.log(numDecodings("227"));
