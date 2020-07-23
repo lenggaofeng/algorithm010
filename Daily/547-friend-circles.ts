ad0b22d741a7519b974fcc63a99d714b0b006ecd
lgfng <747133768@qq.com>
2020-07-24 01:10:20 +0800

    并查集

---
+++ Daily/547-friend-circles.ts
@@ -0,0 +1,14 @@
+/**
+ * https://leetcode-cn.com/problems/friend-circles/
+ */
+
+ /**
+  * todo
+  * 有三种思路， bfs, dfs, 和并查集， 分别实现一遍
+  * 
+  * 
+  *  
+  */
+function findCircleNum(M: number[][]): number {
+
+};
