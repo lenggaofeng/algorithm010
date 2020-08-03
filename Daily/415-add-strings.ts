65610e85e50f8093fe6f0a712e1facf16dc84cdb
lgfng <747133768@qq.com>
2020-08-03 22:14:11 +0800

    daily

---
+++ Daily/415-add-strings.ts
@@ -0,0 +1,40 @@
+/**
+ * 
+ * https://leetcode-cn.com/problems/add-strings/
+ * 
+ */
+
+let code0 = "0".charCodeAt(0);
+let num2Str = new Array(10);
+for(let i = 0; i < 10; i++){
+    num2Str[i] = String.fromCharCode(code0 + i);
+}
+
+function addStrings(num1: string, num2: string): string {
+    if(num1.length > num2.length){
+        let tmp = num1;
+        num1 = num2;
+        num2 = tmp;
+    }
+
+    let m = num1.length;
+    let n = num2.length;
+    let results = new Array(n);
+    let pre = 0;
+    for(let i = 0; i < n ; i++){
+        let n1 = i < m ? num1[m - i - 1].charCodeAt(0) - code0 : 0;
+        let n2 = num2[n - i - 1].charCodeAt(0) - code0;
+        let sum = n1 + n2 + pre;
+        if(sum >= 10){
+            sum -= 10;
+            pre = 1;
+        } else {
+            pre = 0;
+        }
+        results[i] = num2Str[sum];
+    }
+    if(pre){
+        results.push("1");
+    }
+    return results.reverse().join("");
+};
