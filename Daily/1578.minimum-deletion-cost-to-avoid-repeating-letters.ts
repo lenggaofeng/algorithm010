c39fd628a418a0512ce1028b5b95ce64d6d8f0a0
lenggaofeng <lenggaofeng@hortorgames.com>
2020-09-18 10:07:41 +0800

    daily

--- Daily/1578.minimum-deletion-cost-to-avoid-repeating-letters.ts
+++ Daily/1578.minimum-deletion-cost-to-avoid-repeating-letters.ts
@@ -34,21 +34,19 @@ function minCost(s: string, cost: number[]): number {
 
 function minCost1(s: string, cost: number[]): number {
     let n = s.length;
-    let maxCost = 0;
+    let costTotal = 0;
     for (let i = 0; i < n; i ++){
         let c = s[i];
         let max = cost[i];
         while (i < n - 1 && c == s[i + 1]) {
             i++;
             let curCost = cost[i];
-            maxCost += Math.min(max, curCost);
-            if (curCost > max) {
-                max = curCost;
-            }
+            costTotal += Math.min(max, curCost);
+            max = Math.max(max, curCost);
         }
     }
 
-    return maxCost;
+    return costTotal;
 };
 
 export default null;
