929164df541621cb3516f107f2ce4ab32383603d
lenggaofeng <lenggaofeng@hortorgames.com>
2020-10-23 10:36:41 +0800

    daily

---
+++ Daily/234.回文链表.ts
@@ -0,0 +1,53 @@
+/*
+ * @lc app=leetcode.cn id=234 lang=typescript
+ *
+ * [234] 回文链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function isPalindrome(head: ListNode | null): boolean {
+    if(!head || !head.next){ return true;}
+
+    let fast = head;
+    let slow = head;
+    let pre = null;
+    while(fast.next && fast.next.next){
+        fast = fast.next.next;
+        let next = slow.next;
+        slow.next = pre;
+        pre = slow;
+        slow = next;
+    }
+
+    //最后一个结点转想
+    let left = slow;
+    let right = slow.next;
+    slow.next = pre;
+    if(!fast.next){
+        left = left.next;
+    }
+
+    while(left && right){
+        if(left.val != right.val){
+            return false;
+        }
+        left = left.next;
+        right = right.next;
+    }
+
+    return true;
+};
+// @lc code=end
+
